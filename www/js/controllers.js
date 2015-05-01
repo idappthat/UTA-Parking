7fd0ea0e7226bacfa9a38cf2169ab901089b7bad
Cameron Moreau <cameron.cm6@gmail.com>
2015-05-01 15:13:07 -0500

    added erics camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,34 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('DashCtrl', function($scope) {
-	
-})
+.controller('DashCtrl', function($scope, $cordovaCamera) { //tab dash
+
+   $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+ 
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
+}) 
 
 .controller('TabController', function($scope) {
 	
 })
 
 .controller('MapController', function($scope) {
-	$scope.mainMenuSelect = function(b) {
+	$scope.mainMenuSelect = function() {
 		alert("hello " + b);
 	};
 })
