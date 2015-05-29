806aaa1a05f6c1c2fc92e7f5d3355dbc6515d776
Cameron Moreau <cameron.cm6@gmail.com>
2015-05-29 18:02:43 -0500

    Changed to leaflet and fixed marker issues

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,59 @@
-angular.module('starter.controllers', ['ngCordova', 'ngMap'])
+angular.module('starter.controllers', ['ngCordova', 'leaflet-directive'])
 
-.controller('MapViewController', function($scope, $state, Markers, $ionicLoading) {
-  $scope.map = {};
+.controller('MapViewController', function($scope, $state, Markers, $ionicLoading, leafletData) {
+	$scope.map = leafletData.getMap('map');
+	$scope.searchMode = false;
+
+	$scope.mapData = {
+		layers: {
+			baselayers: {
+	            mapbox_light: {
+	                name: 'Mapbox Light',
+	                url: 'https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token={apikey}',
+	                type: 'xyz',
+	                layerOptions: {
+	                    apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+	                    mapid: 'bufanuvols.lia22g09'
+	                }
+	            },
+	            osm: {
+	                name: 'OpenStreetMap',
+	                url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	                type: 'xyz'
+	            }
+            }
+		},
+		center: {
+			lat: 32.730346, 
+			lng: -97.114586,
+			zoom: 17
+		},
+		markers: Markers.parking()
+	};
+
+	$scope.map.then(function(map) {
+		map.attributionControl.setPrefix("");
+	});
+
+
+
+
+
+  /*$scope.map = {};
   $scope.$on('mapInitialized', function(evt, evtMap) {
       $scope.map = evtMap;
   });
-  $scope.markers = Markers.parking();
+  $scope.map.markers = [{
+  	lat: 32.730346,
+  	lng: -97.114586,
+  }];*/
+
+  /*$scope.map.markers[0].setMap($scope.map);
+  //$scope.markers = Markers.parking();
   $scope.map.styles = [{'featureType':'road.highway.controlled_access','elementType':'geometry.fill','stylers':[{'color':'#676d75'}]},{'featureType':'road.highway.controlled_access','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'landscape.natural','elementType':'geometry.fill','stylers':[{'color':'#f8f8f8'}]},{'featureType':'landscape.man_made','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#676D75'}]},{'featureType':'poi','elementType':'geometry.fill','stylers':[{'color':'#cfd5db'}]},{'featureType':'road.highway.controlled_access','elementType':'geometry.fill','stylers':[{'color':'#666666'}]},{'featureType':'road.highway.controlled_access','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road.local','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#ffffff'}]},{'featureType':'road.local','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'visibility':'on'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#676d75'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#585d63'}]},{'featureType':'water','elementType':'geometry.fill','stylers':[{'color':'#43B7EA'}]}];
-  $scope.mainSelected = false;
+  $scope.mainSelected = false;*/
   
-  $scope.centerOnMe = function () {
+  /*$scope.centerOnMe = function () {
     console.log("Centering");
     if (!$scope.map) {
       return;
@@ -28,15 +72,21 @@ angular.module('starter.controllers', ['ngCordova', 'ngMap'])
     }, function (error) {
       alert('Unable to get location: ' + error.message);
     });
-  };
+  };*/
 
 
   $scope.pageSelect = function(b) {
-    if(b == 1) {
-      $scope.map.markers = Markers.parking();
-    } else {
-      $scope.map.markers = [];
-    }
+  	switch(b) {
+  		case 1:
+  			$scope.mapData.markers = Markers.parking();
+  			break;
+		case 4:
+			$scope.mapData.markers = Markers.bikes();
+			break;
+		default:
+			$scope.mapData.markers = {};
+  	}
+    //$scope.mapData.markers = {};
 
     console.log($scope.map.markers);
   };
--- www/js/services.js
+++ www/js/services.js
@@ -1,55 +1,113 @@
 angular.module('starter.services', [])
 
 .factory('Markers', function() {
-  var parkingLots = [
-    { 
-      name: 'Texas Hall',
-      coords: {
-        lat: '32.728830', 
-        lng: '-97.115611'
-      }
-    }, {
-      name: 'Trinity',
-      coords: {
-        lat: '32.730060', 
-        lng: '-97.117277'
-      }
-    }, {
-      name: 'Social Work Parking Lot',
-      coords: {
-        lat: '32.735003', 
-        lng: '-97.113465'
-      }
-    }, {
-      name: 'MAC Parking Lot',
-      coords: {
-        lat: '32.733306', 
-        lng: '-97.117821',
-      }
-    }, {
-      name: 'UH Parking Lot',
-      coords: {
-        lat: '32.727918',
-        lng: '-97.112789'
-      }
-    }, {
-      name: 'Doug Russel Parking Lot',
-      coords: {
-        lat: '32.725869',
-        lng: '-97.112886'
-      }
-    }, {
-      name: 'Tennis Courts Parking Lot',
-      coords: {
-        lat: '32.731186',
-        lng: '-97.119699'
-      }
+  var parkingLots = {
+    m1: { 
+      message: 'Texas Hall',
+      lat: 32.728830, 
+      lng: -97.115611
+    }, 
+    m2: {
+      message: 'Trinity',
+      lat: 32.730060, 
+      lng: -97.117277
+    }, 
+    m3: {
+      message: 'Social Work Parking Lot',
+      lat: 32.735003, 
+      lng: -97.113465
+    }, 
+    m4: {
+      message: 'MAC Parking Lot',
+      lat: 32.733306, 
+      lng: -97.117821,
+    }, 
+    m5: {
+      message: 'UH Parking Lot',
+      lat: 32.727918,
+      lng: -97.112789
+    }, 
+    m6: {
+      message: 'Doug Russel Parking Lot',
+      lat: 32.725869,
+      lng: -97.112886
+    }, 
+    m7: {
+      message: 'Tennis Courts Parking Lot',
+      lat: 32.731186,
+      lng: -97.119699
     }
-  ];
+  };
+
+  var bikeRacks = {
+    m1: { 
+      lat: 32.7302705,
+      lng: -97.11782098 },
+    m2: { 
+      lat: 32.73365495,
+      lng: -97.11591125 },
+    m3: { 
+      lat: 32.73286074,
+      lng: -97.11528897 },
+    m4: { 
+      lat: 32.7341062,
+      lng: -97.11553574 },
+    m5: { 
+      lat: 32.73336614,
+      lng: -97.11720943 },
+    m6: { 
+      lat: 32.73119108,
+      lng: -97.11973071 },
+    m7: { 
+      lat: 32.73356921,
+      lng: -97.12195158 },
+    m8: { 
+      lat: 32.73448073,
+      lng: -97.1131593 },
+    m9: { 
+      lat: 32.73327589,
+      lng: -97.10769296 },
+    m10: { 
+      lat: 32.73262609,
+      lng: -97.10786462 },
+    m11: { 
+      lat: 32.73168296,
+      lng: -97.10786998 },
+    m12: { 
+      lat: 32.72873617,
+      lng: -97.11050391 },
+    m13: { 
+      lat: 32.7279329,
+      lng: -97.10731208 },
+    m14: { 
+      lat: 32.7269491,
+      lng: -97.1091789 },
+    m15: { 
+      lat: 32.72498147,
+      lng: -97.10843861 },
+    m16: { 
+      lat: 32.72604652,
+      lng: -97.11286426 },
+    m17: { 
+      lat: 32.72583893,
+      lng: -97.11055756 },
+    m18: { 
+      lat: 32.72452115,
+      lng: -97.11223125 },
+    m19: { 
+      lat: 32.72324848,
+      lng: -97.11205959 },
+    m20: { 
+      lat: 32.72323043,
+      lng: -97.11055756 },
+  };
 
   return {
     parking: function() {
       return parkingLots;
+    },
+    bikes: function() {
+      return bikeRacks;
     }
   };
 })
